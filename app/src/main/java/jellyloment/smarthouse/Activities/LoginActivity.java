2849322c33c4256082c8c57e28d0ec6bbff7bde1
charly54hp <charly56hp@gmail.com>
2018-10-21 19:34:59 -0700

    Implementando Login
    Log in pide datos para entrar.

--- app/src/main/java/jellyloment/smarthouse/Activities/LoginActivity.java
+++ app/src/main/java/jellyloment/smarthouse/Activities/LoginActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import jellyloment.smarthouse.R;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnIn, btnReg;
-
+    private EditText txtcorreo,txtcontra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +22,13 @@ public class LoginActivity extends AppCompatActivity {
 
         btnIn = findViewById(R.id.btnIniciar);
         btnReg = findViewById(R.id.btnReg);
+        txtcorreo = findViewById(R.id.edtUsuario);
+        txtcontra = findViewById(R.id.edtContrasena);
 
         btnIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+               ChecarUsuario();
             }
         });
         btnReg.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +36,16 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this,RegistroActivity.class));
             }
         });
+
+
+        }
+    public void ChecarUsuario(){
+        if(txtcorreo.getText().toString().equals("") && txtcontra.getText().toString().equals(""))
+        {
+           Toast.makeText(this,"Favor de insertar datos en ambos campos",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
     }
 }
