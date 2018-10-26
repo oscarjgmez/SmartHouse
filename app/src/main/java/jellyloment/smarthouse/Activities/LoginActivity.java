b9f015bc0c1e7f60f421d9e370ac1f2ce7f513d1
charly54hp <charly56hp@gmail.com>
2018-10-26 03:44:42 -0700

    Login Consume datos
    Agrege bibliotecas y la aplicacion ya consume datos de Firebase por el momento solo al usuario(falta contraseña)

--- app/src/main/java/jellyloment/smarthouse/Activities/LoginActivity.java
+++ app/src/main/java/jellyloment/smarthouse/Activities/LoginActivity.java
@@ -1,20 +1,31 @@
 package jellyloment.smarthouse.Activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import jellyloment.smarthouse.R;
 
 public class LoginActivity extends AppCompatActivity {
     private Button btnIn, btnReg;
     private EditText txtcorreo,txtcontra;
+    private String txtMail,txtPass;
+
+    private DatabaseReference mDatabase;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,34 +36,57 @@ public class LoginActivity extends AppCompatActivity {
         txtcorreo = findViewById(R.id.edtUsuario);
         txtcontra = findViewById(R.id.edtContrasena);
 
+
         btnIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                try {
+                ReadFromDatabase();
+                }
+                catch(Exception e)
+                {
 
-               ChecarUsuario();
+                  Toast.makeText(getApplicationContext(),"Huvo un problema con la base de datos",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
         btnReg.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,RegistroActivity.class));
+                startActivity(new Intent(LoginActivity.this, RegistroActivity.class));
             }
         });
+    }
+
+    private void ReadFromDatabase() {
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("user");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.getValue().toString();
 
 
-        }
-    public void ChecarUsuario(){
-        if(!txtcorreo.getText().toString().equals("")  )
-        {
-            if (!txtcontra.getText().toString().equals(""))
-            {
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
-            }
-            else
-                {
-                    Toast.makeText(this,"Favor de insertar datos en ambos campos",Toast.LENGTH_SHORT).show();
+                if (!txtcorreo.getText().toString().equals(name)) {
+
+                    if (!txtcontra.getText().toString().equals("")) {
+
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        //txtcorreo.setText("");
+                        txtcontra.setText("");
+                    } else {
+                        Toast.makeText(LoginActivity.this.getApplicationContext(), "Favor de insertar datos en ambos campos", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this.getApplicationContext(), "Favor de insertar datos en ambos campos", Toast.LENGTH_SHORT).show();
+
                 }
-        }
-        else {
-             Toast.makeText(this,"Favor de insertar datos en ambos campos",Toast.LENGTH_SHORT).show();
+            }
+                @Override
+                public void onCancelled (@NonNull DatabaseError databaseError){
+
+                }
+            });
         }
     }
-}
